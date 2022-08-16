4d43f68e403c3a8ae61ecd6f04a7bc4efb7394f1
natreeum <projectkmscom@gmail.com>
2022-08-16 12:26:36 +0900

    v3.1 update

--- commands/rspbutton.js
+++ commands/rspbutton.js
@@ -3,6 +3,7 @@ const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 const wait = require("node:timers/promises").setTimeout;
 
 const gamedata = new Map();
+let isStarted = false;
 
 const weapons = {
   1: { weakTo: 3, strongTo: 2 },
@@ -28,176 +29,195 @@ module.exports = {
         .setRequired(true)
     ),
   async execute(interaction) {
-    let winner = null;
-
-    //firstuser : who entered command
-    //seconuser : vs
-    firstuser = interaction.user;
-    seconduser = interaction.options.getUser("selectuser");
-
-    // [(firstuser => null),(seconduser => null)]
-    gamedata.set(firstuser, null);
-    gamedata.set(seconduser, null);
-
-    //button builder
-    const row = new ActionRowBuilder()
-      .addComponents(
-        new ButtonBuilder()
-          .setCustomId("scissors")
-          .setLabel("✌")
-          .setStyle(ButtonStyle.Primary)
-      )
-      .addComponents(
-        new ButtonBuilder()
-          .setCustomId("rock")
-          .setLabel("✊")
-          .setStyle(ButtonStyle.Success)
-      )
-      .addComponents(
-        new ButtonBuilder()
-          .setCustomId("paper")
-          .setLabel("✋")
-          .setStyle(ButtonStyle.Danger)
-      );
-
-    //reply to message with buttons
-    await interaction.reply({
-      content: `[✌  ✊  ✋]\n${firstuser}vs${seconduser}\n가위바위보를 시작하지... 아래 버튼을 5초 안에 눌러!!!`,
-      components: [row],
-    });
-
-    //button logic
-    const filter = (i) =>
-      ["scissors", "rock", "paper"].includes(i.customId) &&
-      [firstuser, seconduser].includes(i.user);
-
-    const collector = interaction.channel.createMessageComponentCollector({
-      filter,
-      time: 5000,
-    });
-
-    collector.on("collect", async (i) => {
-      if (i.customId === "scissors") {
-        await i.deferUpdate();
-        // await interaction.followUp(`${i.user}는 버튼을 눌렀어!`);
-        gamedata.set(i.user, 2);
-      } else if (i.customId === "rock") {
-        await i.deferUpdate();
-        // await interaction.followUp(`${i.user}는 버튼을 눌렀어!`);
-        gamedata.set(i.user, 1);
-      } else if (i.customId === "paper") {
-        await i.deferUpdate();
-        // await interaction.followUp(`${i.user}는 버튼을 눌렀어!`);
-        gamedata.set(i.user, 3);
-      }
-    });
-
-    collector.on("end", async (collected) => {
-      //   console.log(
-      //     `${firstuser} : ${gamedata.get(
-      //       firstuser
-      //     )}\n${seconduser} : ${gamedata.get(seconduser)}`
-      //   );
-      function delay(ms) {
-        return new Promise((resolve) => setTimeout(resolve, ms));
-      }
-      await interaction.editReply({
-        content: `${chat[1]}────────${chat[2]}`,
-        components: [],
+    if (isStarted == true) {
+      await interaction.reply({
+        content: `이미 게임이 진행중이야.. 조금만 기다려봐`,
+        ephemeral: true,
       });
-      await delay(100);
-      await interaction.editReply({
-        content: `${chat[2]}────────${chat[3]}`,
-        components: [],
-      });
-      await delay(100);
-      await interaction.editReply({
-        content: `${chat[3]}────────${chat[1]}`,
-        components: [],
-      });
-      await delay(100);
-      await interaction.editReply({
-        content: `${chat[1]}────────${chat[2]}`,
-        components: [],
-      });
-      await delay(100);
-      await interaction.editReply({
-        content: `${chat[2]}────────${chat[3]}`,
-        components: [],
-      });
-      await delay(100);
-      await interaction.editReply({
-        content: `${chat[3]}────────${chat[1]}`,
-        components: [],
-      });
-      await delay(100);
-      await interaction.editReply({
-        content: `${chat[1]}────────${chat[2]}`,
-        components: [],
-      });
+    } else {
+      let winner = null;
+      isStarted = true;
 
-      //1유저가 안눌렀을 때
-      if (
-        gamedata.get(firstuser) === null &&
-        gamedata.get(seconduser) != null
-      ) {
-        winner = "invalid";
-        gamedata.set(firstuser, 4);
-        await interaction.channel.send(
-          `${firstuser}는 쫄았나봐 ㅋㅋㅋ\n에이 재미 없다. 무효!!!`
+      //firstuser : who entered command
+      //seconuser : vs
+      firstuser = interaction.user;
+      seconduser = interaction.options.getUser("selectuser");
+
+      // [(firstuser => null),(seconduser => null)]
+      gamedata.set(firstuser, null);
+      gamedata.set(seconduser, null);
+
+      //button builder
+      const row = new ActionRowBuilder()
+        .addComponents(
+          new ButtonBuilder()
+            .setCustomId("scissors")
+            .setLabel("✌")
+            .setStyle(ButtonStyle.Primary)
+        )
+        .addComponents(
+          new ButtonBuilder()
+            .setCustomId("rock")
+            .setLabel("✊")
+            .setStyle(ButtonStyle.Success)
+        )
+        .addComponents(
+          new ButtonBuilder()
+            .setCustomId("paper")
+            .setLabel("✋")
+            .setStyle(ButtonStyle.Danger)
         );
-      }
-      //2유저가 안눌렀을 때
-      else if (
-        gamedata.get(firstuser) !== null &&
-        gamedata.get(seconduser) === null
-      ) {
-        winner = "invalid";
-        gamedata.set(seconduser, 4);
-        await interaction.channel.send(
-          `${seconduser}는 쫄았나봐 ㅋㅋㅋ\n에이 재미 없다. 무효!!!`
-        );
-      }
-      //둘다 버튼을 안눌렀을 때
-      else if (
-        gamedata.get(firstuser) === null &&
-        gamedata.get(seconduser) === null
-      ) {
-        winner = "invalid";
-        gamedata.set(firstuser, 4);
-        gamedata.set(seconduser, 4);
-        await interaction.channel.send(`뭐야 둘이 게임 안해???`);
-      }
-      //둘 다 뭐라도 냈을 때
-      else {
+
+      //reply to message with buttons
+      await interaction.reply({
+        content: `[✌  ✊  ✋]\n${firstuser}vs${seconduser}\n가위바위보를 시작하지... 아래 버튼을 5초 안에 눌러!!!`,
+        components: [row],
+      });
+
+      //button logic
+      const filter = (i) =>
+        ["scissors", "rock", "paper"].includes(i.customId) &&
+        [firstuser, seconduser].includes(i.user);
+
+      const collector = interaction.channel.createMessageComponentCollector({
+        filter,
+        time: 5000,
+      });
+
+      collector.on("collect", async (i) => {
+        if (i.customId === "scissors") {
+          await i.deferUpdate();
+          // await interaction.followUp(`${i.user}는 버튼을 눌렀어!`);
+          gamedata.set(i.user, 2);
+        } else if (i.customId === "rock") {
+          await i.deferUpdate();
+          // await interaction.followUp(`${i.user}는 버튼을 눌렀어!`);
+          gamedata.set(i.user, 1);
+        } else if (i.customId === "paper") {
+          await i.deferUpdate();
+          // await interaction.followUp(`${i.user}는 버튼을 눌렀어!`);
+          gamedata.set(i.user, 3);
+        }
+      });
+
+      collector.on("end", async (collected) => {
+        //   console.log(
+        //     `${firstuser} : ${gamedata.get(
+        //       firstuser
+        //     )}\n${seconduser} : ${gamedata.get(seconduser)}`
+        //   );
+        function delay(ms) {
+          return new Promise((resolve) => setTimeout(resolve, ms));
+        }
+        await interaction.editReply({
+          content: `${chat[1]}────────${chat[2]}`,
+          components: [],
+        });
+        await delay(100);
+        await interaction.editReply({
+          content: `${chat[2]}────────${chat[3]}`,
+          components: [],
+        });
+        await delay(100);
+        await interaction.editReply({
+          content: `${chat[3]}────────${chat[1]}`,
+          components: [],
+        });
+        await delay(100);
+        await interaction.editReply({
+          content: `${chat[1]}────────${chat[2]}`,
+          components: [],
+        });
+        await delay(100);
+        await interaction.editReply({
+          content: `${chat[2]}────────${chat[3]}`,
+          components: [],
+        });
+        await delay(100);
+        await interaction.editReply({
+          content: `${chat[3]}────────${chat[1]}`,
+          components: [],
+        });
+        await delay(100);
+        await interaction.editReply({
+          content: `${chat[1]}────────${chat[2]}`,
+          components: [],
+        });
+
+        //1유저가 안눌렀을 때
         if (
-          weapons[gamedata.get(firstuser)].weakTo === gamedata.get(seconduser)
-        )
-          winner = seconduser;
+          gamedata.get(firstuser) === null &&
+          gamedata.get(seconduser) != null
+        ) {
+          winner = "invalid";
+          gamedata.set(firstuser, 4);
+          await interaction.channel.send(
+            `${firstuser}는 쫄았나봐 ㅋㅋㅋ\n에이 재미 없다. 무효!!!`
+          );
+        }
+        //2유저가 안눌렀을 때
         else if (
-          weapons[gamedata.get(firstuser)].strongTo === gamedata.get(seconduser)
-        )
-          winner = firstuser;
-        else winner = "DRAW";
-      }
-      if (winner === "DRAW") {
-        await interaction.channel.send(
-          `${firstuser} : ${chat[gamedata.get(firstuser)]} - ${seconduser} : ${
-            chat[gamedata.get(seconduser)]
-          }\n오~ 둘이 통했나본데~ 비겼어!!`
-        );
-      } else if (winner === "invalid") {
-        await interaction.channel.send(
-          `${firstuser} : ${chat[gamedata.get(firstuser)]} - ${seconduser} : ${
-            chat[gamedata.get(seconduser)]
-          }\n이번 게임은 무효야!!`
-        );
-      } else
-        await interaction.channel.send(
-          `${firstuser} : ${chat[gamedata.get(firstuser)]} - ${seconduser} : ${
-            chat[gamedata.get(seconduser)]
-          }\nWinner : ${winner}`
-        );
-    });
+          gamedata.get(firstuser) !== null &&
+          gamedata.get(seconduser) === null
+        ) {
+          winner = "invalid";
+          gamedata.set(seconduser, 4);
+          await interaction.channel.send(
+            `${seconduser}는 쫄았나봐 ㅋㅋㅋ\n에이 재미 없다. 무효!!!`
+          );
+        }
+        //둘다 버튼을 안눌렀을 때
+        else if (
+          gamedata.get(firstuser) === null &&
+          gamedata.get(seconduser) === null
+        ) {
+          winner = "invalid";
+          gamedata.set(firstuser, 4);
+          gamedata.set(seconduser, 4);
+          await interaction.channel.send(`뭐야 둘이 게임 안해???`);
+        }
+        //둘 다 뭐라도 냈을 때
+        else {
+          if (
+            weapons[gamedata.get(firstuser)].weakTo === gamedata.get(seconduser)
+          )
+            winner = seconduser;
+          else if (
+            weapons[gamedata.get(firstuser)].strongTo ===
+            gamedata.get(seconduser)
+          )
+            winner = firstuser;
+          else winner = "DRAW";
+        }
+        if (winner === "DRAW") {
+          await interaction.channel.send(
+            `${firstuser} : ${
+              chat[gamedata.get(firstuser)]
+            } - ${seconduser} : ${
+              chat[gamedata.get(seconduser)]
+            }\n오~ 둘이 통했나본데~ 비겼어!!`
+          );
+          isStarted = false;
+        } else if (winner === "invalid") {
+          await interaction.channel.send(
+            `${firstuser} : ${
+              chat[gamedata.get(firstuser)]
+            } - ${seconduser} : ${
+              chat[gamedata.get(seconduser)]
+            }\n이번 게임은 무효야!!`
+          );
+          isStarted = false;
+        } else {
+          await interaction.channel.send(
+            `${firstuser} : ${
+              chat[gamedata.get(firstuser)]
+            } - ${seconduser} : ${
+              chat[gamedata.get(seconduser)]
+            }\nWinner : ${winner}`
+          );
+          isStarted = false;
+        }
+      });
+    }
   },
 };
